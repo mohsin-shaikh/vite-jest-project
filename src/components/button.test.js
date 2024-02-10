aa53a46518bc6b30cd50c84c92b91522dd848595
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2024-02-10 17:48:19 +0530

    New Test Cases

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -2,15 +2,6 @@
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import Button from './button';
 
-
-// test('handle click', async () => {
-//   const onClick = jest.fn();
-//   render(<Button>Refresh</Button>);
-//   const buttonElement = await screen.findByText(/Refresh/i);
-//   fireEvent.click(buttonElement);
-//   expect(onClick).toHaveBeenCalledTimes(1);
-// });
-
 // afterEach function runs after each test suite is executed
 afterEach(() => {
   cleanup(); // Resets the DOM after each test suite
